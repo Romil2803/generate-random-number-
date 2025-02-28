b2ad043d06b9cdb643dd6c6855512c6ab7ff88fb
Romil Sangani <ssanganiromil@gamil.com>
2025-02-28 10:49:39 +0530

    fix error

--- js/app.js
+++ js/app.js
@@ -1,101 +1,3 @@
-// const cabins = [
-//   { cabin: 1, spots: [1, 2, 3, 4, 5, 6], nameCount: 0, maxNames: 6 }, 
-//   { cabin: 2, spots: [7, 8, 9, 10, 11, 12], nameCount: 0, maxNames: 6 }, 
-//   { cabin: 3, spots: [13, 14,15, 16, 17, 18], nameCount: 0, maxNames: 6 } 
-// ];
-// function storeName() {
-//   const nameInput = document.getElementById("name").value;
-
-//   let storedNames = JSON.parse(localStorage.getItem('storedNames')) || [];
-
-//   if (storedNames.length >= 18) {
-//     alert("You can only store up to 18 names.");
-//     document.getElementById("name").value = "";
-//     return;
-//   }
-
-//   if (nameInput !== "") {
-//     storedNames.push(nameInput); 
-//     localStorage.setItem('storedNames', JSON.stringify(storedNames));  
-
-//     document.getElementById("name").value = "";
-
-//     displayStoredNames();
-//   }
-// }
-
-
-// function setCabinSpots(cabinNumber) {
-//   const spotsInput = document.getElementById(`cabin${cabinNumber}-spots`).value;
-//   const cabin = cabins[cabinNumber - 1];
-//   if (spotsInput >= 1 && spotsInput <= cabin.spots.length) {
-//     cabin.maxNames = spotsInput;
-//     cabin.nameCount = 0; 
-//   } else {
-//     alert("Please enter a valid number of spots.");
-//   }
-// }
-
-// function assignAndDisplayNames() {
-//   const cabin1Val = document.getElementById("cabin1-spots").value;
-// const cabin2Val = document.getElementById("cabin2-spots").value;
-// const cabin3Val = document.getElementById("cabin3-spots").value;
-// const totelVal=cabin1Val+cabin2Val+cabin3Val;
-
-//   const names = JSON.parse(localStorage.getItem('storedNames'));
-
-//   if (!names || names.length === 0) {
-//     alert("No names stored! Please store names first.");
-//     return;
-//   }
-//   const shuffledNames = [...names];
-//   for (let i = shuffledNames.length - 1; i > 0; i--) {
-//     const j = Math.floor(Math.random() * (i + 1));
-//     [shuffledNames[i], shuffledNames[j]] = [shuffledNames[j], shuffledNames[i]];
-//   }
-//   let nameIndex = 0;
-//   cabins.forEach(cabin => {
-//     for (let i = 0; i < cabin.spots.length && nameIndex < shuffledNames.length && cabin.nameCount < cabin.maxNames; i++) {
-//       if (nameIndex < shuffledNames.length) {
-//         cabin.spots[i] = `${cabin.spots[i]}: ${shuffledNames[nameIndex++]}`;
-//         cabin.nameCount++;
-//       }
-//     }
-//   });
-//   displayCabins();
-// }
-
-
-// function displayCabins() {
-//   const cabinsContainer = document.getElementById('cabins');
-//   cabinsContainer.innerHTML = '';
-
-//   cabins.forEach(cabin => {
-//     const cabinDiv = document.createElement('div');
-//     cabinDiv.classList.add('cabin');
-//     cabinDiv.innerHTML = `Cabin ${cabin.cabin}`;
-
-   
-//     cabin.spots.forEach(spot => {
-//       const spotDiv = document.createElement('div');
-//       spotDiv.classList.add('spot');
-//       spotDiv.innerHTML = `${spot}`; 
-//       cabinDiv.appendChild(spotDiv);
-//     });
-
-//     cabinsContainer.appendChild(cabinDiv);
-//   });
-// }
-// function refreshPage(){
-//   window.location.reload();
-// } 
-
-// // function displayStoredNames() {
-// //   const storedNames = JSON.parse(localStorage.getItem('storedNames')) || [];
-// //   const namesListDiv = document.getElementById('stored-names-list');
-// //   namesListDiv.innerHTML = "<strong>Stored Names:</strong><br>" + storedNames.join(", ");
-// // }
-
 const cabins = [
   { cabin: 1, spots: [1, 2, 3, 4, 5, 6], names: [] },
   { cabin: 2, spots: [1, 2, 3, 4, 5, 6], names: [] },
@@ -169,20 +71,20 @@ function assignAndDisplayNames() {
     }
   });
   
-  rotateNames();
-
   displayCabins();
+  // rotateNames();
+
 }
 
-function rotateNames() {
-  const firstCabinNames = cabins[0].names.slice(); 
-  const secondCabinNames = cabins[1].names.slice(); 
-  const thirdCabinNames = cabins[2].names.slice();
+// function rotateNames() {
+//   const firstCabinNames = cabins[0].names.slice(); 
+//   const secondCabinNames = cabins[1].names.slice(); 
+//   const thirdCabinNames = cabins[2].names.slice();
 
-  cabins[0].names = secondCabinNames;
-  cabins[1].names = thirdCabinNames;
-  cabins[2].names = firstCabinNames;
-}
+//   // cabins[0].names = secondCabinNames;
+//   // cabins[1].names = thirdCabinNames;
+//   // cabins[2].names = firstCabinNames;
+// }
 
 function displayCabins() {
   const cabinsContainer = document.getElementById('cabins');
